e6110f092e3d84dd44540e5e622f6bfd5587662d
Walter Herold Veedla <walterheroldv@gmail.com>
2023-11-25 16:13:47 +0100

    Move DNSanswer from dnsmessage.py to dns_message/dns_answer.py

--- app/dnsmessage.py
+++ app/dnsmessage.py
@@ -6,69 +6,9 @@ from app import dns_record_type
 from app import dns_record_class
 from app.dns_message.dns_header import DNSheader
 from app.dns_message.dns_question import DNSquestion
+from app.dns_message.dns_answer import DNSanswer
 
 NETWORK_BYTE_ORDER = 'big'
-
-@dataclass
-class DNSanswer:
-    name: list[str] 
-    record_type: int
-    clazz: int
-    ttl: int
-    rdlength: int
-    rdata: str
-
-    @classmethod
-    def from_message(cls, message: bytes) -> Self:
-        # TODO HARDCODED
-        name = ['codecrafters', 'io']
-        record_type = dns_record_type.RecordType.A.value
-        clazz = dns_record_class.RecordClass.IN.value
-        # ttl = struct.pack('!I', 60)
-        ttl = 60
-        # rdlength = struct.pack('!H', 4)
-        rdata = '8.8.8.8'
-
-        return cls(name, record_type, clazz, ttl, len(rdata)-3, rdata)
-
-    def pack(self) -> bytes:
-        """
-        Packs the DNS answer fields into a bytes object suitable for network transmission.
-        Returns:
-            bytes: The packed DNS answer as a byte string.
-        
-        """
-        name = b''
-
-        for label in self.name:
-            name += struct.pack('!B', len(label)) + label.encode()
-        name += b'\x00'
-
-        # convert rdata to integer format
-        rdata_encoded = b''
-        for ip_part in '8.8.8.8'.split('.'):
-            rdata_encoded += struct.pack('!B', int(ip_part))
-
-        # pack fixed length fields: type, class, TTL length and data
-        format_str = '!HHIH'
-        packed_fields = struct.pack(format_str, 
-            self.record_type,
-            self.clazz,
-            self.ttl,
-            self.rdlength,
-        )
-
-        return name + packed_fields + rdata_encoded
-
-    def __str__(self) -> str:
-        return (f'DNSanswer:\n'
-            f'\tname: {self.name}\n'
-            f'type: {self.record_type}\n'
-            f'class: {self.clazz}\n'
-            f'ttl: {self.ttl}\n'
-            f'rdlength: {self.rdlength}\n'
-            f'rdata: {self.rdata}\n'
-        )
 
 
 
